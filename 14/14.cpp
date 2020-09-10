a26c8531b511048b8710f921befc9f13094439bc
wuyudi <wuyudi1109@gmail.com>
2020-09-11 01:52:19 +0800

    Create 14.cpp

---
+++ 14/14.cpp
@@ -0,0 +1,44 @@
+#pragma GCC optimize(3, "Ofast", "inline")
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+map<uint32_t, uint32_t> sol;
+ll maxi = 0, maxcnt = 0;
+int main()
+{
+    for (size_t i = 1; i <= 1000000; i++)
+    {
+        auto temp = i;
+        int cnt = 1;
+        bool flag = true;
+        while (temp != 1)
+        {
+            if (sol.count(temp))
+            {
+                sol[i] = cnt + sol[temp] - 1;
+                flag = false;
+                break;
+            }
+            if (temp % 2 == 0)
+            {
+                temp /= 2;
+            }
+            else
+            {
+                temp = temp * 3 + 1;
+            }
+            cnt++;
+        }
+        if (flag)
+        {
+            sol[i] = cnt;
+        }
+        if (maxcnt < sol[i])
+        {
+            maxcnt = sol[i];
+            maxi = i;
+        }
+    }
+    cout << maxi;
+    return 0;
+}
