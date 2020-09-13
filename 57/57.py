72b5517ad16f6c67edf6faf1927fa78ad115fec4
wuyudi <wuyudi1109@gmail.com>
2020-09-13 15:33:26 +0800

    Create 57.py

---
+++ 57/57.py
@@ -0,0 +1,12 @@
+from fractions import Fraction
+
+
+def frac():
+    n = Fraction(1, 2)
+    while True:
+        yield n+1
+        n = 1/(2+n)
+
+
+sum(1 for i, j in zip(range(1000), frac())
+    if len(str(j.numerator)) > len(str(j.denominator)))
