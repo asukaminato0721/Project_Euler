f2934512b5dcc08bededaa766030de5c65eb5854
wuyudi <wuyudi1109@gmail.com>
2020-09-16 14:18:50 +0800

    Update .cpp

--- 24/.cpp
+++ 24/.cpp
@@ -5,10 +5,8 @@ int main()
 {
     auto i = vector<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int n = 1000000 - 1;
-    while (n--)
-    {
-        next_permutation(i.begin(), i.end());
-    }
+    for (; n; next_permutation(i.begin(), i.end()), n--)
+        ;
     for_each(i.begin(), i.end(), [](const auto &p) { cout << p << " "; });
     return 0;
 }
