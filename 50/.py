3c96cfddf385c52d333a8c9cb7c6dfc083359c95
wuyudi <wuyudi1109@gmail.com>
2020-09-20 01:54:07 +0800

    Create .py

---
+++ 50/.py
@@ -0,0 +1,20 @@
+from sympy import primerange, isprime
+from collections import defaultdict
+from itertools import accumulate
+from operator import add
+
+num = defaultdict(int)
+
+
+def foo(start=1):
+    for i, j in enumerate(accumulate(primerange(start, 100_0000), add)):
+        if j > 100_0000:
+            break
+        if isprime(j):
+            num[i] = j
+
+
+for k in range(1, 100):
+    foo(k)
+
+a = max(num.items(), key=lambda x: x[0])
