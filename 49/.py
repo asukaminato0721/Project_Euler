afef3ea9b189ff06cbe08f1dd09c3f9bd991e04f
wuyudi <wuyudi1109@gmail.com>
2020-09-20 00:31:01 +0800

    Update .py

--- 49/.py
+++ 49/.py
@@ -1,5 +1,6 @@
 from sympy import primerange
 from itertools import combinations
-for i, j, k in combinations(primerange(1000, 10000), 3):
-    if sorted(str(i)) == sorted(str(j)) == sorted(str(k)) and i+k == 2*j:
-        print(i, j, k)
+prime = set(primerange(1000, 10000))
+for i, k in combinations(prime, 2):
+    if sorted(str(i)) == sorted(str(m := ((i+k)//2))) == sorted(str(k)) and m in prime:
+        print(*sorted((i, k, m)), sep='')
