7414c6e8d6c22d98e3d21da196c1756ed07f3278
wuyudi <wuyudi1109@gmail.com>
2020-09-20 22:31:33 +0800

    Update .py

--- 12/.py
+++ 12/.py
@@ -1,9 +1,9 @@
 from itertools import accumulate
 from operator import add
-from sympy.ntheory import divisor_count
+from diofant.ntheory import divisor_count
+from itertools import count
 
-
-for i in accumulate(range(10000000), add):
+for i in accumulate(count(1), add):
     if divisor_count(i) > 500:
         print(i)
         break
