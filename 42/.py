ecc72089de2e6d0ddd3d2622bbea664a6f912b3a
wuyudi <wuyudi1109@gmail.com>
2020-09-27 12:26:12 +0800

    Create .py

---
+++ 42/.py
@@ -0,0 +1,14 @@
+import requests
+
+tri = {n*(n+1)//2 for n in range(1, 1000)}
+
+text = requests.get(
+    "https://projecteuler.net/project/resources/p042_words.txt"
+).text.replace('\"', "").split(',')
+
+
+def num(a):
+    return ord(a)-ord('A')+1
+
+
+print(sum(1 for j in text if (sum(num(i) for i in j) in tri)))
