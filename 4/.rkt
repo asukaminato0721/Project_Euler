33a4c21e2b4f95ec1e4dccef5de0dd76cda5f292
wuyudi <wuyudi1109@gmail.com>
2020-09-29 19:39:49 +0800

    Update .rkt

--- 4/.rkt
+++ 4/.rkt
@@ -1,7 +1,7 @@
 #lang racket
 (require srfi/13)
 (define (=? a)(string=? (string-reverse (~a a)) (~a a)))
-(foldl (lambda (a b) (max a b))
+(foldl max
        1
        (filter =? (map (lambda (a) (* (car a) (cadr a)))
                        (cartesian-product (range 999 100 -1) (range 999 100 -1)))))
