7da384f5b8df51a470812536bd13218f4c6427d8
wuyudi <wuyudi1109@gmail.com>
2020-11-20 22:48:29 +0800

    Update .py

--- 12/.py
+++ 12/.py
@@ -3,7 +3,4 @@ from operator import add
 from diofant.ntheory import divisor_count
 from itertools import count
 
-for i in accumulate(count(1), add):
-    if divisor_count(i) > 500:
-        print(i)
-        break
+print(next(i for i in accumulate(count(1), add) if divisor_count(i) > 500))
