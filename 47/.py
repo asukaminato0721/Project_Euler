1f6c6d5a0c5dc761846bacb973e48a28e590fac3
wuyudi <wuyudi1109@gmail.com>
2020-11-20 22:40:31 +0800

    Update .py

--- 47/.py
+++ 47/.py
@@ -1,6 +1,4 @@
 from diofant.ntheory import factorint as FactorInteger
 from itertools import count
-for i in count(1):
-    if all(len(FactorInteger(n)) == 4 for n in range(i, i+4)):
-        print(i)
-        break
+
+print(next(i for i in count(1) if all(len(FactorInteger(n)) == 4 for n in range(i, i + 4))))
