b390747ec9af294021d28d007cb172a0139d014a
wuyudi <wuyudi1109@gmail.com>
2020-11-20 22:12:18 +0800

    Update .py

--- 45/.py
+++ 45/.py
@@ -1,4 +1,4 @@
-poly3 = set(i*(i+1)//2 for i in range(1, 1000000))
-poly5 = set(i*(3*i-1)//2 for i in range(1, 1000000))
-poly6 = set(i*(2*i-1) for i in range(1, 1000000))
-set.intersection(poly3, poly6, poly5)
+poly3 = set(i * (i + 1) // 2 for i in range(1, 1000000))
+poly5 = set(i * (3 * i - 1) // 2 for i in range(1, 1000000))
+poly6 = set(i * (2 * i - 1) for i in range(1, 1000000))
+print(poly3 & poly5 & poly6)
