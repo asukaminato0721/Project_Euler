abe9cd2c0cd22a3b135c2bdd0ec4edd666745d65
wuyudi <wuyudi1109@gmail.com>
2020-11-20 22:42:26 +0800

    Update 52.py

--- 52/52.py
+++ 52/52.py
@@ -2,11 +2,7 @@ from itertools import count
 
 
 def iff(n):
-    same = sorted(str(n))
-    return all(sorted(str(i)) == same for i in range(n, 7*n, n))
+    return all(sorted(str(i)) == sorted(str(n)) for i in range(n, 7 * n, n))
 
 
-for i in count(1):
-    if iff(i):
-        print(i)
-        break
+print(next(i for i in count(1) if iff(i)))
