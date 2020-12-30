d76ca07bb4775989b1fe670faedd4cb7b2a3a03a
wuyudi <wuyudi119@163.com>
2020-12-30 12:39:36 +0800

    docs: :truck: add vaild filename

--- 49/49.py
+++ 49/49.py
@@ -1,6 +1,7 @@
 from diofant import primerange
 from itertools import combinations
+
 prime = set(primerange(1000, 10000))
 for i, k in combinations(prime, 2):
-    if sorted(str(i)) == sorted(str(m := ((i+k)//2))) == sorted(str(k)) and m in prime:
-        print(*sorted((i, k, m)), sep='')
+    if sorted(str(i)) == sorted(str(m := ((i + k) // 2))) == sorted(str(k)) and m in prime:
+        print(*sorted((i, k, m)), sep="")
